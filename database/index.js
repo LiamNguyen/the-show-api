425eac246c6292128618706cb415a69d2a8a9dd9
Liam Nguyen <phuc.nguyen2@metropolia.fi>
2018-03-29 22:34:49 +0300

    Final update for database connection string

--- database/index.js
+++ database/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 exports.connect = () => {
   mongoose.connect(
-    `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@
-    ${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@` +
+    `${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     { useMongoClient: true, promiseLibrary: global.Promise }
   );
   const db = mongoose.connection;
